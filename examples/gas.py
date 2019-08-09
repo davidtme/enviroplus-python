ba016216c965273d298bde2814bfb946e8dc0e13
David Teasdale <david@teasdale.me>
2019-08-09 19:59:26 +0100

    print format

--- examples/gas.py
+++ examples/gas.py
@@ -12,13 +12,13 @@ Press Ctrl+C to exit!
 try:
     while True:
         in0 = gas.adc.get_voltage('in0/ref')
-        print(in0)
+        print('in0: {in0}')
 
         in1 = gas.adc.get_voltage('in1/ref')
-        print(in1)
+        print('in1: {in1}')
     
-        in1 = gas.adc.get_voltage('in2/ref')
-        print(in2)
+        in2 = gas.adc.get_voltage('in2/ref')
+        print('in2: {in2}')
 
         time.sleep(1.0)
 except KeyboardInterrupt:
